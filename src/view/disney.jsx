4623e291dde0f4490bda85f25b53b2054c78966b
a39635322 <a39635322@gmail.com>
2025-03-05 16:59:42 +0800

    阿蘇確定要製作的頁面

--- src/view/disney.jsx
+++ src/view/disney.jsx
@@ -3,7 +3,7 @@ import {Outlet} from "react-router"
 
 function disney() {
     return (<>
-        <h1>迪士尼頁面</h1>
+        <h1>迪士尼頁面，組員阿蘇要將就內容轉移至此</h1>
         /*網頁程式碼請放這裡面 */
         <Outlet />
     </>)
