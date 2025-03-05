ac3d67bfd42b278f400157c91dcf117d89103d6e
Joanne Wei <joannewei0921@gmail.com>
2025-03-05 22:52:23 +0800

    update header&footer

---
+++ src/helpers/routes.js
@@ -0,0 +1,10 @@
+export const routes = [
+    {
+      path: "/search",
+      name: "景點列表",
+    },
+    {
+      path: "/about",
+      name: "關於我們",
+    },
+  ];
--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,13 +1,61 @@
-import React from "react"
+import React from "react";
+import { routes } from "../helpers/routes";
+import { NavLink } from "react-router";
 
 function footer() {
-    return (<>
-        <h1>footer</h1>
-        /*網頁程式碼請放這裡面 */
-        
-    </>)
+  return (
+    <>
+      <footer className="footer navbar-expand-lg ">
+        <div className="container-xxl py-120">
+          <div className="d-flex align-items-center gap-11">
+            <a href="./index.html">
+              <picture>
+                <source
+                  media="(max-width: 991px)"
+                  srcSet="/images/logo-small.png"
+                />
+                <img src="/images/logo.png" alt="A responsive image" />
+              </picture>
+            </a>
+            <ul className="nav d-flex gap-3">
+              {routes.map((route) => (
+                <li className="nav-item">
+                  <NavLink key={route.path}
+                    className="nav-link fs-6 py-3 px-6 text-black"
+                    to={route.path}
+                  >
+                    {route.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+            <div className="social-media-group d-flex gap-3">
+              <div className="social-media fs-4">
+                <i className="bi bi-facebook"></i>
+              </div>
+              <div className="social-media fs-4">
+                <i className="bi bi-instagram"></i>
+              </div>
+              <div className="social-media fs-4">
+                <i className="bi bi-linkedin"></i>
+              </div>
+              <div className="social-media fs-4">
+                <i className="bi bi-twitter"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-black py-4">
+          <p className="text-center fs-7 text-white mb-0">
+            Copyright 2024 日本四季物語 All Rights Reserved.
+          </p>
+          <p className="text-center fs-7 text-white mb-0">
+            僅供學習使用,無商業用途。
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-
-
-export default footer
+export default footer;
